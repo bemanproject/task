1c3d79bd21f0c981d1752319eab347d4f8dda015
Dietmar Kühl <dietmar.kuehl@me.com>
2025-06-23 23:19:16 +0100

    created a draft outline responding to the current concerns

--- examples/issue-symmetric-transfer.cpp
+++ examples/issue-symmetric-transfer.cpp
@@ -1,23 +1,27 @@
-// examples/hello.cpp                                                 -*-C++-*-
+// examples/issue-symmetric-transfer.cpp                              -*-C++-*-
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 #include <beman/execution/task.hpp>
 #include <beman/execution/execution.hpp>
 #include <iostream>
+#include <functional>
 
 namespace ex = beman::execution;
+
+template <typename Env>
+ex::task<void, Env> test() {
+    for (std::size_t i{}; i < 1000000; ++i) {
+        co_await std::invoke([]()->ex::task<> {
+            co_await ex::just();
+        });
+    }
+}
 
 int main() {
     struct affine_env {};
     struct inline_env {
         using scheduler_type = ex::inline_scheduler;
     };
-    return std::get<0>(ex::sync_wait([]() -> ex::task<int, inline_env> {
-            for (std::size_t i{}; i < 1000000; ++i) {
-                co_await std::invoke([]()->ex::task<> {
-                    co_await ex::just();
-                });
-            }
-            co_return 0;
-        }()).value_or(std::tuple(-1)));
+    ex::sync_wait(test<affine_env>()); // OK
+    //ex::sync_wait(test<inline_env>()); // error: stack overflow
 }
