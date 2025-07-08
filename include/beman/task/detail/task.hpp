1f3fa0faaaf0482230facd97f3f3c9198db7479b
Dietmar Kühl <dietmar.kuehl@me.com>
2025-07-08 22:33:18 +0100

    clang format

--- include/beman/task/detail/task.hpp
+++ include/beman/task/detail/task.hpp
@@ -46,7 +46,7 @@ class task {
 
   public:
     using task_concept           = int;
-    using sender_concept        = ::beman::execution::sender_t;
+    using sender_concept         = ::beman::execution::sender_t;
     using xcompletion_signatures = ::beman::execution::detail::meta::combine<
         ::beman::execution::completion_signatures<beman::task::detail::completion_t<Value>,
                                                   ::beman::execution::set_stopped_t()>,
