6e652c1a277388424ee5dd7b9785f918a8d4b17e
Edward Nolan <eddiejnolan@gmail.com>
2025-08-24 09:15:13 -0400

    Update beman-submodules and bring infra back in sync with upstream
    Pull request 43 (https://github.com/bemanproject/task/pull/43) removed
    compatibility with MSVC AddressSanitizer builds. The way this change
    should have been applied would have been to omit the "Release.MaxSan"
    build from the "msvc" build configuration and remove the "msvc-debug"
    preset from "ci_tests.yml" and CMakePresets.json, but instead the
    infra/cmake/msvc-toolchain.cmake file was modified to remove the
    sanitizer flag, making it out of sync with upstream infra.
    
    This should have been caught by the beman-submodule CI check, but was
    not due to a bug:
    https://github.com/bemanproject/infra-workflows/commit/962bc39d246b4dd3e1fc34250a66d188610c30e7
    
    This commit updates the ci_tests.yml file appropriately and updates
    both beman-submodules in this repository to the latest version.
    
    It also applies the exclusion rule to the CMake linter added by
    https://github.com/bemanproject/infra/commit/0a25bfae2e24eacc67b16180de95c5dbb3ab2b8e.

--- infra/tools/beman-tidy/tests/lib/checks/beman_standard/readme/test_readme.py
+++ infra/tools/beman-tidy/tests/lib/checks/beman_standard/readme/test_readme.py
@@ -11,9 +11,12 @@ from tests.utils.path_runners import (
 
 # Actual tested checks.
 from beman_tidy.lib.checks.beman_standard.readme import (
+    ReadmePurposeCheck,
     ReadmeTitleCheck,
     ReadmeBadgesCheck,
+    ReadmeImplementsCheck,
     ReadmeLibraryStatusCheck,
+    ReadmeLicenseCheck,
 )
 
 test_data_prefix = "tests/lib/checks/beman_standard/readme/data"
@@ -21,7 +24,14 @@ valid_prefix = f"{test_data_prefix}/valid"
 invalid_prefix = f"{test_data_prefix}/invalid"
 
 
-def test__README_TITLE__valid(repo_info, beman_standard_check_config):
+def test__readme_purpose__is_always_skipped(repo_info, beman_standard_check_config):
+    """
+    Test that readme.purpose is always skipped, as it cannot be implemented.
+    """
+    assert ReadmePurposeCheck(repo_info, beman_standard_check_config).should_skip()
+
+
+def test__readme_title__valid(repo_info, beman_standard_check_config):
     """
     Test that a valid README.md title passes the check.
     """
@@ -45,16 +55,19 @@ def test__README_TITLE__valid(repo_info, beman_standard_check_config):
     )
 
 
-def test__README_TITLE__invalid(repo_info, beman_standard_check_config):
+def test__readme_title__invalid(repo_info, beman_standard_check_config):
     """
     Test that an invalid README.md title fails the check.
     """
     invalid_readme_paths = [
+        # Title: Wrong Title Format
         Path(f"{invalid_prefix}/invalid.md"),
+        # Title: Missing . in beman.exemplar
         Path(f"{invalid_prefix}/invalid-title-v1.md"),
+        # Title: Missing : after beman.exemplar
         Path(f"{invalid_prefix}/invalid-title-v2.md"),
+        # Title: Wromg name beman.exemaplar vs beman.optional
         Path(f"{invalid_prefix}/invalid-title-v3.md"),
-        Path(f"{invalid_prefix}/invalid-title-v4.md"),
     ]
 
     run_check_for_each_path(
@@ -66,7 +79,7 @@ def test__README_TITLE__invalid(repo_info, beman_standard_check_config):
     )
 
 
-def test__README_TITLE__fix_inplace(repo_info, beman_standard_check_config):
+def test__readme_title__fix_inplace(repo_info, beman_standard_check_config):
     """
     Test that the fix method corrects an invalid README.md title.
     """
@@ -74,7 +87,6 @@ def test__README_TITLE__fix_inplace(repo_info, beman_standard_check_config):
         Path(f"{invalid_prefix}/invalid-title-v1.md"),
         Path(f"{invalid_prefix}/invalid-title-v2.md"),
         Path(f"{invalid_prefix}/invalid-title-v3.md"),
-        Path(f"{invalid_prefix}/invalid-title-v4.md"),
     ]
 
     run_fix_inplace_for_each_file_path(
@@ -82,14 +94,18 @@ def test__README_TITLE__fix_inplace(repo_info, beman_standard_check_config):
     )
 
 
-def test__README_BADGES__valid(repo_info, beman_standard_check_config):
+def test__readme_badges__valid(repo_info, beman_standard_check_config):
     """
     Test that a valid README.md badges passes the check.
     """
     valid_readme_paths = [
+        # Badges: under development status and cpp26 target
         Path(f"{valid_prefix}/README-v1.md"),
+        # Badges: production ready (api may undergo changes) status and cpp26 target
         Path(f"{valid_prefix}/README-v2.md"),
+        # Badges: production ready (stable api) status and cpp29 target
         Path(f"{valid_prefix}/README-v3.md"),
+        # Badges: retired status and cpp26 target
         Path(f"{valid_prefix}/README-v4.md"),
     ]
 
@@ -102,15 +118,28 @@ def test__README_BADGES__valid(repo_info, beman_standard_check_config):
     )
 
 
-def test__README_BADGES__invalid(repo_info, beman_standard_check_config):
+def test__readme_badges__invalid(repo_info, beman_standard_check_config):
     """
     Test that an invalid README.md badges fails the check.
     """
     invalid_readme_paths = [
         Path(f"{invalid_prefix}/invalid.md"),
+        # Badges: typos in badge for library status
         Path(f"{invalid_prefix}/invalid-badge-v1.md"),
+        # Badges: typos in badge for standard target
         Path(f"{invalid_prefix}/invalid-badge-v2.md"),
+        # Badges: other description in badge for library status
         Path(f"{invalid_prefix}/invalid-badge-v3.md"),
+        # Badges: other description in badge for standard target
+        Path(f"{invalid_prefix}/invalid-badge-v4.md"),
+        # Badges: non-sense badge for library status (broken badge URL)
+        Path(f"{invalid_prefix}/invalid-badge-v5.md"),
+        # Badges: non-sense badge for standard target (broken badge URL)
+        Path(f"{invalid_prefix}/invalid-badge-v6.md"),
+        # Badges: 1/2 badges are missing (standard target)
+        Path(f"{invalid_prefix}/invalid-badge-v7.md"),
+        # Badges: 1/2 badges are missing (library status)
+        Path(f"{invalid_prefix}/invalid-badge-v8.md"),
     ]
 
     run_check_for_each_path(
@@ -122,15 +151,66 @@ def test__README_BADGES__invalid(repo_info, beman_standard_check_config):
     )
 
 
-@pytest.mark.skip(reason="NOT implemented")
-def test__README_BADGES__fix_inplace(repo_info, beman_standard_check_config):
+@pytest.mark.skip(reason="not implemented")
+def test__readme_badges__fix_inplace(repo_info, beman_standard_check_config):
     """
     Test that the fix method corrects an invalid README.md badges.
+    Note: Skipping this test as it is not implemented.
     """
     pass
 
 
-def test__README_LIBRARY_STATUS__valid(repo_info, beman_standard_check_config):
+def test__readme_implements__valid(repo_info, beman_standard_check_config):
+    """
+    Test that a valid README.md "Implements" passes the check
+    """
+    valid_readme_paths = [
+        Path(f"{valid_prefix}/README-v1.md"),
+        Path(f"{valid_prefix}/README-v2.md"),
+        Path(f"{valid_prefix}/README-v3.md"),
+        Path(f"{valid_prefix}/README-v4.md"),
+    ]
+
+    run_check_for_each_path(
+        True,
+        valid_readme_paths,
+        ReadmeImplementsCheck,
+        repo_info,
+        beman_standard_check_config,
+    )
+
+
+def test__readme_implements__invalid(repo_info, beman_standard_check_config):
+    """
+    Test that an invalid README.md "Implements" fails the check
+    """
+    invalid_readme_paths = [
+        Path(f"{invalid_prefix}/invalid.md"),
+        Path(f"{invalid_prefix}/invalid-implements-v1.md"),
+        Path(f"{invalid_prefix}/invalid-implements-v2.md"),
+        Path(f"{invalid_prefix}/invalid-implements-v3.md"),
+        Path(f"{invalid_prefix}/invalid-implements-v4.md"),
+    ]
+
+    run_check_for_each_path(
+        False,
+        invalid_readme_paths,
+        ReadmeImplementsCheck,
+        repo_info,
+        beman_standard_check_config,
+    )
+
+
+@pytest.mark.skip(reason="not implemented")
+def test__readme_implements__fix_inplace(repo_info, beman_standard_check_config):
+    """
+    Test that the fix method corrects an invalid README.md "Implements".
+    Note: Skipping this test as it is not implemented.
+    """
+    pass
+
+
+def test__readme_library_status__valid(repo_info, beman_standard_check_config):
     """
     Test that a valid README.md library status passes the check.
     """
@@ -150,7 +230,7 @@ def test__README_LIBRARY_STATUS__valid(repo_info, beman_standard_check_config):
     )
 
 
-def test__README_LIBRARY_STATUS__invalid(repo_info, beman_standard_check_config):
+def test__readme_library_status__invalid(repo_info, beman_standard_check_config):
     """
     Test that an invalid README.md library status fails the check.
     """
@@ -170,9 +250,65 @@ def test__README_LIBRARY_STATUS__invalid(repo_info, beman_standard_check_config)
     )
 
 
-@pytest.mark.skip(reason="NOT implemented")
-def test__README_LIBRARY_STATUS__fix_inplace(repo_info, beman_standard_check_config):
+@pytest.mark.skip(reason="not implemented")
+def test__readme_library_status__fix_inplace(repo_info, beman_standard_check_config):
     """
     Test that the fix method corrects an invalid README.md library status.
+    Note: Skipping this test as it is not implemented.
+    """
+    pass
+
+
+def test__readme_license__valid(repo_info, beman_standard_check_config):
+    """
+    Test that a valid README.md license passes the check.
+    """
+    valid_readme_paths = [
+        # License: Apache License 2.0 with LLVM Exceptions
+        Path(f"{valid_prefix}/README-v1.md"),
+        # License: Boost Software License 1.0
+        Path(f"{valid_prefix}/README-v2.md"),
+        # License: MIT License
+        Path(f"{valid_prefix}/README-v3.md"),
+        # License: Apache License 2.0 with LLVM Exceptions and MIT License
+        Path(f"{valid_prefix}/README-v4.md"),
+    ]
+
+    run_check_for_each_path(
+        True,
+        valid_readme_paths,
+        ReadmeLicenseCheck,
+        repo_info,
+        beman_standard_check_config,
+    )
+
+
+def test__readme_license__invalid(repo_info, beman_standard_check_config):
+    """
+    Test that an invalid README.md license fails the check.
+    """
+    invalid_readme_paths = [
+        # License: Missing license section
+        Path(f"{invalid_prefix}/invalid-license-section-v1.md"),
+        # License: Empty license section
+        Path(f"{invalid_prefix}/invalid-license-section-v2.md"),
+        # License: Invalid license
+        Path(f"{invalid_prefix}/invalid-license-section-v3.md"),
+    ]
+
+    run_check_for_each_path(
+        False,
+        invalid_readme_paths,
+        ReadmeLicenseCheck,
+        repo_info,
+        beman_standard_check_config,
+    )
+
+
+@pytest.mark.skip(reason="not implemented")
+def test__readme_license__fix_inplace(repo_info, beman_standard_check_config):
+    """
+    Test that the fix method corrects an invalid README.md license.
+    Note: Skipping this test as it is not implemented.
     """
     pass
